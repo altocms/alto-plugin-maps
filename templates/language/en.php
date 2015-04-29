8520697df5861621cb8138cef4d35ff2ad6c5323
aVadim483 <vadim483@gmail.com>
2015-04-29 22:01:36 +0300

    logo

---
+++ templates/language/en.php
@@ -0,0 +1,19 @@
+<?php
+/*-------------------------------------------------------
+*
+*   Plugin "Maps"
+*   Author: Vladimir Yuriev (extravert)
+*   Official site: altocms.ru
+*   Contact e-mail: support@lsmods.ru
+*
+---------------------------------------------------------
+* Adopted for Alto CMS v.1.+ by aVadim
+*/
+ 
+return array(
+
+    'map' => 'Map',
+    
+);
+
+// EOF
